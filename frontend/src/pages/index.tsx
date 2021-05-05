4affa762e27387aa5527523799cb6c4683161826
Gleydson Delcho <gleydson.engenhariacivil@gmail.com>
2021-05-05 19:00:42 -0300

    Create new frontend with next

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>DSVendas</title>
+        <meta name="description" content="Projeto desenvolido na semana spring React" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>  
+      <h1>Hello Vendas</h1>
+    </div>  
+  )
+}
