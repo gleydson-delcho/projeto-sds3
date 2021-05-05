8c83e9f3bcacf0914c7a22b7efcf53be2071b9e6
Gleydson Delcho <gleydson.engenhariacivil@gmail.com>
2021-05-05 19:41:42 -0300

    Bootstrap

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,4 +1,4 @@
-
+import '../styles/css/global.scss';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,14 +1,15 @@
 import Head from 'next/head'
 
+
 export default function Home() {
   return (
-    <div>
+    <div className="">
       <Head>
         <title>DSVendas</title>
         <meta name="description" content="Projeto desenvolido na semana spring React" />
         <link rel="icon" href="/favicon.ico" />
       </Head>  
-      <h1>Hello Vendas</h1>
+      <h1 className="text-primary">Hello Vendas</h1>
     </div>  
   )
 }
