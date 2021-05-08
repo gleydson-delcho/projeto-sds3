00a97fc3b0de877c6ae0ed718fb9a739cd823b10
Gleydson Delcho <gleydson.engenhariacivil@gmail.com>
2021-05-08 16:47:42 -0300

    Modify dotenv var

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -2,5 +2,5 @@ import axios from 'axios';
 import 'dotenv/config'
 
 export const api = axios.create({
-    baseURL: process.env.BACKEND_URL ?? 'http://localhost:8080'
+    baseURL: process.env.NODE_ENV_BACKEND_URL ?? 'http://localhost:8080'
 });
