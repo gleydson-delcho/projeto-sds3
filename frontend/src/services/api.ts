db2033d656a7c6bb14ddbbe02e0bf352124ac051
Gleydson Delcho <gleydson.engenhariacivil@gmail.com>
2021-05-08 18:14:00 -0300

    Modify dotenv var

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_BACKEND_URL ?? 'http://localhost:8080'
+    baseURL: process.env.REACT_APP_NEXT_PUBLIC_BACKEND_URL ?? 'http://localhost:8080'
 });
 
